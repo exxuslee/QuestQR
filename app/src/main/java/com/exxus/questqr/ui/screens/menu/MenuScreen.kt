aa12b15fc46b72e7bf5dfce1d6bc38015d730156
Exxuslee <exxuslee@gmail.com>
2023-12-16 22:31:10 +0200

    add photo

--- app/src/main/java/com/exxus/questqr/ui/screens/menu/MenuScreen.kt
+++ app/src/main/java/com/exxus/questqr/ui/screens/menu/MenuScreen.kt
@@ -1,24 +1,30 @@
 package com.exxus.questqr.ui.screens.menu
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.exxus.questqr.MyPreferences
+import com.exxus.questqr.R
 import com.exxus.questqr.ui.navigation.Screen
 import com.exxus.questqr.ui.theme.QuestQRTheme
+
 
 @Composable
 fun MenuScreen(navController: NavHostController) {
@@ -27,30 +33,41 @@ fun MenuScreen(navController: NavHostController) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
-    )
-    {
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.main),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp)
-                .background(Color.Gray),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "QR", modifier = Modifier.padding(48.dp))
-            Button(onClick = {
-                val myPreferences = MyPreferences(context)
-                myPreferences.saveBoolean("reverse", true)
-                navController.navigate(Screen.Scanner.route)
-            }) {
-                Text(text = "Na game")
+            Spacer(modifier = Modifier.height(164.dp))
+            Button(
+                modifier = Modifier.padding(end = 164.dp),
+                onClick = {
+                    val myPreferences = MyPreferences(context)
+                    myPreferences.saveBoolean("reverse", true)
+                    navController.navigate(Screen.Scanner.route)
+                }) {
+                Text(
+                    text = "Nadiya",
+                    fontSize = 18.sp
+                )
             }
-
-            Button(onClick = {
-                val myPreferences = MyPreferences(context)
-                myPreferences.saveBoolean("reverse", false)
-                navController.navigate(Screen.Scanner.route)
-            }) {
-                Text(text = "Ni game")
+            Spacer(modifier = Modifier.height(100.dp))
+            Button(
+                modifier = Modifier.padding(start = 164.dp),
+                onClick = {
+                    val myPreferences = MyPreferences(context)
+                    myPreferences.saveBoolean("reverse", false)
+                    navController.navigate(Screen.Scanner.route)
+                }) {
+                Text(
+                    text = "Nikita",
+                    fontSize = 18.sp
+                )
             }
         }
     }
